216dd5a0d22ebd73090452f38afd1791a40c846a
zulkeflipolimas <zulkeflipolimas@gmail.com>
2026-02-25 10:50:51 +0800

    Update mqtt_task2.ino

--- PW4/mqtt_task2.ino
+++ PW4/mqtt_task2.ino
@@ -2,14 +2,14 @@
 #include <PubSubClient.h>      // 🌐 Library MQTT
 
 // ================= WIFI CONFIG =================
-#define WIFI_SSID "ExtendVoltrabot"
-#define WIFI_PASSWORD "r@w@1985"
+#define WIFI_SSID "_______________"      // 📶 Nama WiFi
+#define WIFI_PASSWORD "_______________"         // 🔐 Password WiFi
 
 // ================= MQTT CONFIG =================
-#define MQTT_HOST "broker.emqx.io"
-#define MQTT_PORT 1883
+#define MQTT_HOST "_______________"        // 🌍 MQTT Broker
+#define MQTT_PORT 1883                   // Port MQTT
 
-#define SUB_TOPIC "write_your_own_Topic_1"
+#define SUB_TOPIC "_______________" // 📩 Topic SUBSCRIBE
 
 // ================= HARDWARE =================
 #define LED1_PIN 18            // 💡 LED 1
